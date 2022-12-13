4ed8b109ee19a91ab7091a490f88bd91ab8494c1
Roy <95645767+RoyNkem@users.noreply.github.com>
2022-12-14 00:57:48 +0100

    Create Custom UITabBar to use for tab item navigation. The tab bar is drawn using bezier curve path

---
+++ MintynApp/Controllers/TabBar/MyTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MyTabBarController.swift
+//  MintynApp
+//
+//  Created by Roy Aiyetin on 13/12/2022.
+//
+
+import UIKit
+
+class MyTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
